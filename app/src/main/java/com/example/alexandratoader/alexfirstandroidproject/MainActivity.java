32dad7fd45b3f33f29786bce1ff1df8cfdea6cdd
Alexandra Toader <alexandratoader@Alexandras-MBP.guest.lan>
2017-04-19 21:45:52 +0200

    added fields to enter address of the place, category and name. Refactored the name of the id and field.

--- app/src/main/java/com/example/alexandratoader/alexfirstandroidproject/MainActivity.java
+++ app/src/main/java/com/example/alexandratoader/alexfirstandroidproject/MainActivity.java
@@ -3,23 +3,26 @@ package com.example.alexandratoader.alexfirstandroidproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText enterDestinationField;
+    EditText enterAddress;
+    EditText enterCategory;
+    EditText enterName;
     TextView firstTextView;
     TextView secondTextView;
     TextView thirdTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_entry);
-        enterDestinationField = (EditText) findViewById(R.id.enter_destination_field);
+        enterAddress = (EditText) findViewById(R.id.enter_address);
+        enterCategory = (EditText) findViewById(R.id.enter_category);
+        enterName = (EditText) findViewById(R.id.enter_name);
         firstTextView = (TextView) findViewById(R.id.first_text_view);
         secondTextView = (TextView) findViewById(R.id.second_text_view);
         thirdTextView = (TextView) findViewById(R.id.third_text_view);
@@ -28,14 +31,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addItem(View view) {
-        String destinationField= enterDestinationField.getText().toString();
-        if(firstTextView.getText().toString().isEmpty()){
-        firstTextView.setText(destinationField);
-        } else if (secondTextView.getText().toString().isEmpty()){
-            secondTextView.setText(destinationField);
-        } else if (thirdTextView.getText().toString().isEmpty()){
-            thirdTextView.setText(destinationField);
-        }
+        String address= enterAddress.getText().toString();
+        String category= enterCategory.getText().toString();
+        String name= enterName.getText().toString();
+        firstTextView.setText(address);
+        secondTextView.setText(category);
+        thirdTextView.setText(name);
 
 
 
